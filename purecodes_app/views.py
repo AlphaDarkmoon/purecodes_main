b27ed2baad8e211b774739577d3883bed86dc4df
AlphaDarkmoon <darkmode.alpha@gmail.com>
2023-10-13 17:41:00 +0530

    Added Search function & minor updates 🫐

--- purecodes_app/views.py
+++ purecodes_app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from purecodes_app.models import Post
 
+from django.db.models import Q
+
 def home_view(request):
+    query = request.GET.get('q','')  # Get the search query from the URL parameter 'q'
     latest_posts = Post.objects.all().order_by('-created_at')
-    return render(request, "purecodes_app/home.html", {'latest_posts': latest_posts})
+
+    if query:
+        # If a search query is provided, filter the posts by title containing the query
+        latest_posts = latest_posts.filter(title__icontains=query)
+
+    elif query == 'None':
+        query = ''
+
+    return render(request, "purecodes_app/home.html", {'latest_posts': latest_posts, 'query': query})
+
